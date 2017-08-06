ebfbee70796e6e2eff78c5f7946070e93218cee0
Don Kelly <karfai@gmail.com>
2017-08-06 19:06:03 -0400

    Gets envelope from document service
    Change-Id: I4d8f33efaf916dc32c6ed42c0cfcbb7b7abf5e1e

--- lib/envelope.rb
+++ lib/envelope.rb
@@ -1,9 +1,20 @@
+require 'xa/clients/documents'
+
 require_relative './worker'
 
 class Envelope < Worker
   from_queue :envelope
 
   def event(o)
-    p o
+    # get the document's envelope
+    Sneakers.logger.info("# requesting envelope for document (id=#{o['id']})")
+    envelope = client.envelope(o['id'])
+    Sneakers.logger.info("# received envelope")
+  end
+
+  private
+
+  def client
+    @client ||= XA::Clients::Documents.new('http://localhost:3000')
   end
 end
